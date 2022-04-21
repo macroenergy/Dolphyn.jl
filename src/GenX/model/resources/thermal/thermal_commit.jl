81dee452b69e6354683ea0552aa05d276cd4faf6
Jun Law <junlaw@mit.edu>
2022-04-21 18:30:57 -0400

    Update thermal_commit.jl

--- src/GenX/model/resources/thermal/thermal_commit.jl
+++ src/GenX/model/resources/thermal/thermal_commit.jl
@@ -360,10 +360,19 @@ function thermal_commit_reserves(EP::Model, inputs::Dict)
 	return EP
 end
 
-@doc raw"""
-    hoursbefore(p::Int, t::Int, b::UnitRange)
 
-This is a generalization of hoursbefore(... b::Int)
+@doc raw"""
+	hoursbefore(p::Int, t::Int, b::UnitRange)
+
+Determines the time index b hours before index t in
+a landscape starting from t=1 which is separated
+into distinct periods of length p.
+
+For example, if p = 10,
+1 hour before t=1 is t=10,
+1 hour before t=10 is t=9
+1 hour before t=11 is t=20
+
 to allow for example b=1:3 to fetch a Vector{Int} of the three hours before
 time index t.
 """
