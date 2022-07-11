95a4577e0869d36637a4b85155317f051ef9ed81
Yuheng Zhang <2845024327@qq.com>
2022-07-12 01:10:54 +0800

    sbatch test on hpc

---
+++ Example_Systems/SomeWhere/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name="CSC_NO_COMMIT_TEST_2022_07_12"
+#SBATCH --output="CSC_NO_COMMIT_TEST_2022_07_12.%A.%a.out"
+#SBATCH --error="CSC_NO_COMMIT_TEST_2022_07_12.%A.%a.err"
+
+#SBATCH --mail-type=fail
+#SBATCH --mail-user="2845024327@qq.com"
+
+#SBATCH -p C032M0128G
+#SBATCH -qos=low
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 8
+
+module load ~/modulefiles/julia/1.7.3
+module load ~/modulefiles/gurobi/9.1.2
+
+julia Run.jl
