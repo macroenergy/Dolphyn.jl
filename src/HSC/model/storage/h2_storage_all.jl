3221dc063bcd5a908aa44e55942441029090f8fe
dharik13 <dharik13@gmail.com>
2022-09-14 15:05:09 -0400

    Updated h2_storage_all.jl
    - check if H2_STOR_DURATION is non-empty before enforcing a constraint over that set.

--- src/HSC/model/storage/h2_storage_all.jl
+++ src/HSC/model/storage/h2_storage_all.jl
@@ -146,7 +146,7 @@ function h2_storage_all(EP::Model, inputs::Dict, setup::Dict)
 	# Links state of charge in first time step with decisions in last time step of each subperiod
 	# We use a modified formulation of this constraint (cSoCBalLongDurationStorageStart) when operations wrapping and long duration storage are being modeled
 	
-	if setup["OperationWrapping"] == 1 && !isempty(H2_STOR_LONG_DURATION) # Apply constraints to those storage technologies with short duration only
+	if setup["OperationWrapping"] == 1 && !isempty(H2_STOR_LONG_DURATION)  && !isempty(H2_STOR_SHORT_DURATION) # Apply constraints to those storage technologies with short duration only (if non-empty)
 		@constraint(EP, cH2SoCBalStart[t in START_SUBPERIODS, y in H2_STOR_SHORT_DURATION], EP[:vH2S][y,t] ==
 			EP[:vH2S][y,t+hours_per_subperiod-1]-(1/dfH2Gen[!,:H2Stor_eff_discharge][y]*EP[:vH2Gen][y,t])
 			+(dfH2Gen[!,:H2Stor_eff_charge][y]*EP[:vH2_CHARGE_STOR][y,t])-(dfH2Gen[!,:H2Stor_self_discharge_rate_p_hour][y]*EP[:vH2S][y,t+hours_per_subperiod-1]))
