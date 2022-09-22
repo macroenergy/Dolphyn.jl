416938f516a68a5881e030909c357c6681939ddc
Junwenlaw <97320660+Junwenlaw@users.noreply.github.com>
2022-09-21 23:49:49 -0400

    Update h2_storage_investment_energy.jl

--- src/HSC/model/storage/h2_storage_investment_energy.jl
+++ src/HSC/model/storage/h2_storage_investment_energy.jl
@@ -33,7 +33,7 @@ The total energy capacity of storage resource is defined as the sum of the exist
 In addition, this module adds investment and fixed OM costs related to energy capacity to the objective function:
 ```math
 \begin{equation*}
-	\sum_{s \in \mathcal{S}} \sum_{z \in \mathcal{Z}} (c_{s,z}^{\textrm{H,ENE,INV}}} \times y_{s,z}^{\textrm{H,ENE,new}} + \textrm{c}_{s,z}^{\textrm{H,ENE,FOM}} \times y_{y,z}^{\textrm{H,ENE,total}})
+	\sum_{s \in \mathcal{S}} \sum_{z \in \mathcal{Z}} (c_{s,z}^{\textrm{H,ENE,INV}} \times y_{s,z}^{\textrm{H,ENE,new}} + \textrm{c}_{s,z}^{\textrm{H,ENE,FOM}} \times y_{y,z}^{\textrm{H,ENE,total}})
 \end{equation*}
 ```
 
