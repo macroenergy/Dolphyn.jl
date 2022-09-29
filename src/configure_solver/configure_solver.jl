9e045149d9a68eac0ac93f875bae8d8ae6e9fb69
Yuheng Zhang <2845024327@qq.com>
2022-09-29 19:41:18 +0800

    drop scip

--- src/configure_solver/configure_solver.jl
+++ src/configure_solver/configure_solver.jl
@@ -20,40 +20,36 @@ received this license file.  If not, see <http://www.gnu.org/licenses/>.
 This method returns a solver-specific MathOptInterface OptimizerWithAttributes optimizer instance to be used in the GenX.generate\_model() method.
 
 The "solver" argument is a string which specifies the solver to be used. It is not case sensitive.
-Currently supported solvers include: "Gurobi", "CPLEX", "Clp", "Cbc", or "SCIP"
+Currently supported solvers include: "Gurobi", "CPLEX", "Clp", "Cbc", or "HiGHS"
 
 The "solver\_settings\_path" argument is a string which specifies the path to the directory that contains the settings YAML file for the specified solver.
 
 """
 function configure_solver(solver::String, solver_settings_path::String)
 
-	solver = lowercase(solver)
+    solver = lowercase(solver)
 
-	# Set solver as HiGHS
-	if solver == "highs"
-		highs_settings_path = joinpath(solver_settings_path, "highs_settings.yml")
-        	OPTIMIZER = configure_highs(highs_settings_path)
-	# Set solver as Gurobi
-	elseif solver == "gurobi"
-		gurobi_settings_path = joinpath(solver_settings_path, "gurobi_settings.yml")
-        	OPTIMIZER = configure_gurobi(gurobi_settings_path)
-	# Set solver as CPLEX
-	elseif solver == "cplex"
-		cplex_settings_path = joinpath(solver_settings_path, "cplex_settings.yml")
-        	OPTIMIZER = configure_cplex(cplex_settings_path)
-	# Set solver as Clp
-	elseif solver == "clp"
-		clp_settings_path = joinpath(solver_settings_path, "clp_settings.yml")
-        	OPTIMIZER = configure_clp(clp_settings_path)
-	# Set solver as Cbc
-	elseif solver == "cbc"
-		cbc_settings_path = joinpath(solver_settings_path, "cbc_settings.yml")
-        	OPTIMIZER = configure_cbc(cbc_settings_path)
-	# Set solver as SCIP
-	elseif solver == "scip"
-		scip_settings_path = joinpath(solver_settings_path, "scip_settings.yml")
-		OPTIMIZER = configure_scip(scip_settings_path)
-	end
+    # Set solver as HiGHS
+    if solver == "highs"
+        highs_settings_path = joinpath(solver_settings_path, "highs_settings.yml")
+        OPTIMIZER = configure_highs(highs_settings_path)
+        # Set solver as Gurobi
+    elseif solver == "gurobi"
+        gurobi_settings_path = joinpath(solver_settings_path, "gurobi_settings.yml")
+        OPTIMIZER = configure_gurobi(gurobi_settings_path)
+        # Set solver as CPLEX
+    elseif solver == "cplex"
+        cplex_settings_path = joinpath(solver_settings_path, "cplex_settings.yml")
+        OPTIMIZER = configure_cplex(cplex_settings_path)
+        # Set solver as Clp
+    elseif solver == "clp"
+        clp_settings_path = joinpath(solver_settings_path, "clp_settings.yml")
+        OPTIMIZER = configure_clp(clp_settings_path)
+        # Set solver as Cbc
+    elseif solver == "cbc"
+        cbc_settings_path = joinpath(solver_settings_path, "cbc_settings.yml")
+        OPTIMIZER = configure_cbc(cbc_settings_path)
+    end
 
-	return OPTIMIZER
+    return OPTIMIZER
 end
