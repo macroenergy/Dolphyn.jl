d6e727c8bbc3fd62145ed38781028aa372738262
RuaridhMacd <rmacd@mit.edu>
2022-11-28 01:29:56 -0500

    Update julenv.jl

--- julenv.jl
+++ julenv.jl
@@ -32,21 +32,21 @@ function get_gurobi_version()
 end
 
 Pkg.activate("DOLPHYNJulEnv")
-Pkg.add(Pkg.PackageSpec(name="Cbc", version="0.8.0"))
-Pkg.add(Pkg.PackageSpec(name="Clp", version="0.8.4"))
+Pkg.add(Pkg.PackageSpec(name="Cbc", version="1.0.1"))
+Pkg.add(Pkg.PackageSpec(name="Clp", version="1.0.1"))
+Pkg.add(Pkg.PackageSpec(name="SCIP", version="0.11.3"))
 Pkg.add(Pkg.PackageSpec(name="HiGHS", version="1.1.4"))
-Pkg.add(Pkg.PackageSpec(name="DataStructures", version="0.18.9"))
+Pkg.add(Pkg.PackageSpec(name="DataStructures", version="0.18.13"))
 Pkg.add(Pkg.PackageSpec(name="Dates"))
 Pkg.add(Pkg.PackageSpec(name="GLPK", version="0.14.12"))
 Pkg.add(Pkg.PackageSpec(name="Ipopt", version="0.7.0"))
-Pkg.add(Pkg.PackageSpec(name="JuMP", version="0.21.8"))
-Pkg.add("MathOptInterface")
-#Pkg.add(Pkg.PackageSpec(name="CPLEX", version="0.7.7"))
-Pkg.add(Pkg.PackageSpec(name="CSV", version="0.8.5"))
+Pkg.add(Pkg.PackageSpec(name="JuMP", version="1.1.1"))
+Pkg.add(Pkg.PackageSpec(name="MathOptInterface", version="1.6.1"))
+Pkg.add(Pkg.PackageSpec(name="CSV", version="0.10.4"))
 Pkg.add(Pkg.PackageSpec(name="Clustering", version="0.14.2"))
 Pkg.add(Pkg.PackageSpec(name="Combinatorics", version="1.0.2"))
-Pkg.add(Pkg.PackageSpec(name="Distances", version="0.10.3"))
-Pkg.add(Pkg.PackageSpec(name="DataFrames", version="1.0.0")) #0.20.2
+Pkg.add(Pkg.PackageSpec(name="Distances", version="0.10.7"))
+Pkg.add(Pkg.PackageSpec(name="DataFrames", version="1.3.4")) #0.20.2
 Pkg.add(Pkg.PackageSpec(name="Documenter", version="0.27.3"))
 Pkg.add(Pkg.PackageSpec(name="DocumenterTools", version="0.1.13"))
 gurobi_ver = get_gurobi_version()
@@ -62,16 +62,18 @@ Pkg.build("Gurobi")
 
 ##Add if elseif with Method of Morris for these
 Pkg.add(Pkg.PackageSpec(name="DiffEqSensitivity", version="6.52.1"))
-Pkg.add(Pkg.PackageSpec(name="Statistics"))
+Pkg.add(Pkg.PackageSpec(name="Statistics", version="1.4.0"))
 Pkg.add(Pkg.PackageSpec(name="OrdinaryDiffEq", version="5.60.1"))
 Pkg.add(Pkg.PackageSpec(name="QuasiMonteCarlo", version="0.2.3"))
 ##Add if elseif with Method of Morris for these
-Pkg.add(Pkg.PackageSpec(name="BenchmarkTools", version="1.1.1"))
+Pkg.add(Pkg.PackageSpec(name="BenchmarkTools", version="1.3.1"))
 Pkg.add(Pkg.PackageSpec(name="MathProgBase", version="0.7.8"))
-Pkg.add(Pkg.PackageSpec(name="StatsBase", version="0.33.8"))
+Pkg.add(Pkg.PackageSpec(name="StatsBase", version="0.33.21"))
 Pkg.add(Pkg.PackageSpec(name="YAML", version="0.4.7"))
 Pkg.add(Pkg.PackageSpec(name="LinearAlgebra"))
 Pkg.add(Pkg.PackageSpec(name="Revise"))
+Pkg.add(Pkg.PackageSpec(name="RecursiveArrayTools", version="2.31.2")
+
 
 # Logging
 Pkg.add(Pkg.PackageSpec(name="Logging"))
