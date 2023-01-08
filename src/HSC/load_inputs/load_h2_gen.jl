3ba0b52bfbb092ba27f631a14e3f7a48d4681560
Yuheng Zhang <2845024327@qq.com>
2023-01-08 15:41:56 +0800

    select zone in hydrogen generators

--- src/HSC/load_inputs/load_h2_gen.jl
+++ src/HSC/load_inputs/load_h2_gen.jl
@@ -21,8 +21,13 @@ Function for reading input parameters related to hydrogen generators.
 """
 function load_h2_gen(setup::Dict, path::AbstractString, sep::AbstractString, inputs_gen::Dict)
 
+	Zones = inputs_gen["Zones"]
+
 	#Read in H2 generation related inputs
     h2_gen_in = DataFrame(CSV.File(string(path,sep,"HSC_generation.csv"), header=true), copycols=true)
+
+	# Filter H2 generation by zone
+	h2_gen_in = filter(row -> row.Zone in Zones, h2_gen_in)
 
     # Add Resource IDs after reading to prevent user errors
 	h2_gen_in[!,:R_ID] = 1:size(collect(skipmissing(h2_gen_in[!,1])),1)
