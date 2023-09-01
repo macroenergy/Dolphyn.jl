75ecf993c3206fe5a0753bd2ec64b2732d6e54a1
shaker <shaker@mit.edu>
2023-09-01 07:18:11 +0000

    added missing h2_pipeline expression back

--- src/HSC/model/transmission/h2_pipeline.jl
+++ src/HSC/model/transmission/h2_pipeline.jl
@@ -187,6 +187,8 @@ function h2_pipeline(EP::Model, inputs::Dict, setup::Dict)
 
     EP[:eH2Balance] += ePipeZoneDemand
 
+    EP[:eHTransmissionByZone] += ePipeZoneDemand
+
     ## End Balance Expressions ##
     ### End Expressions ###
 
