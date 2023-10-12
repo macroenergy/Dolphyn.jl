34a613c043dbf38d7829ea797e289c313d2beaaf
bennettm <yuanwei2@hotmail.com>
2023-10-12 07:22:11 -0400

    Correct zone values in h2_pipeline_flow.csv file (#184)
    Co-authored-by: Mary Bennett <mbennett@mitadms-MBP.home>

--- src/HSC/write_outputs/write_h2_pipeline_flow.jl
+++ src/HSC/write_outputs/write_h2_pipeline_flow.jl
@@ -30,21 +30,21 @@ function write_h2_pipeline_flow(path::AbstractString, sep::AbstractString, input
     dfPowerBalance = Array{Any}
     rowoffset=3
     for p in 1:H2_P
-           dfTemp1 = Array{Any}(nothing, T+rowoffset, 3)
-           dfTemp1[1,1:size(dfTemp1,2)] = ["Source_Zone_H2_Net", 
-               "Sink_Zone_H2_Net", "Pipe_Level"]
+        dfTemp1 = Array{Any}(nothing, T+rowoffset, 3)
+        dfTemp1[1,1:size(dfTemp1,2)] = ["Source_Zone_H2_Net", "Sink_Zone_H2_Net", "Pipe_Level"]
 
-           dfTemp1[2,1:size(dfTemp1,2)] = repeat([p],size(dfTemp1,2))
+        dfTemp1[2,1:size(dfTemp1,2)] = repeat([p],size(dfTemp1,2))
         
-        dfTemp1[3,1:size(dfTemp1,2)] = [[H2_Pipe_Map[(H2_Pipe_Map[!,:d] .== 1) .& (H2_Pipe_Map[!,:pipe_no] .== p), :][!,:zone_str][1]],[H2_Pipe_Map[(H2_Pipe_Map[!,:d] .== -1) .& (H2_Pipe_Map[!,:pipe_no] .== p), :][!,:zone_str][1]],"NA"]
+        zone_value1 = filter.(isdigit, ([H2_Pipe_Map[(H2_Pipe_Map[!,:d] .== 1) .& (H2_Pipe_Map[!,:pipe_no] .== p), :][!,:zone_str][1]][1]))
+        zone_value2 = filter.(isdigit, ([H2_Pipe_Map[(H2_Pipe_Map[!,:d] .== -1) .& (H2_Pipe_Map[!,:pipe_no] .== p), :][!,:zone_str][1]][1])) 
+        dfTemp1[3,1:size(dfTemp1,2)] = [zone_value1,zone_value2,"NA"]
 
-           for t in 1:T
-             dfTemp1[t+rowoffset,1]= value.(EP[:eH2PipeFlow_net][p,t,1])
-             dfTemp1[t+rowoffset,2] = value.(EP[:eH2PipeFlow_net][p,t,-1])
+        for t in 1:T
+            dfTemp1[t+rowoffset,1]= value.(EP[:eH2PipeFlow_net][p,t,1])
+            dfTemp1[t+rowoffset,2] = value.(EP[:eH2PipeFlow_net][p,t,-1])
             dfTemp1[t+rowoffset,3] = value.(EP[:vH2PipeLevel][p,t])
-             
-            
-           end
+        end
+
         if p==1
             dfPowerBalance =  hcat(vcat(["", "Pipe", "Zone"], ["t$t" for t in 1:T]), dfTemp1)
         else
