dc0646d8860fe331c2226e9b9d939493c7bef0a9
Ruaridh Macdonald <rmacd@mit.edu>
2023-11-02 13:17:48 -0400

    Update package name

--- src/DOLPHYN.jl
+++ src/DOLPHYN.jl
@@ -14,7 +14,7 @@ in LICENSE.txt.  Users uncompressing this from an archive may not have
 received this license file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-module DOLPHYN
+module Dolphyn
 
 #export package_activate
 export compare_results
