38da33ec79267e9283003a18c702a06a97444632
Ruaridh Macdonald <rmacd@mit.edu>
2023-11-22 00:38:54 -0500

    Fix new bug in thermal_commit
    The behaviour of the sum() function was changed in a new version of JuMP. This has been patched in GenX so I've copied the change here till we decide to update all of GenX.
    
    I also corrected the load file of the CSC/ThreeZones example

--- src/Dolphyn.jl
+++ src/Dolphyn.jl
@@ -135,6 +135,7 @@ genx_to_exclude = [
     joinpath(genxsubmod_path,"write_outputs","write_subsidy_revenue.jl"),
     joinpath(genxsubmod_path,"model","core","emissions.jl"),
     joinpath(genxsubmod_path,"model","policies","co2_cap.jl"),
+    joinpath(genxsubmod_path,"model","resources","thermal","thermal_commit.jl"),
     # joinpath(genxsubmod_path,"configure_settings") # DOLPHYN and GenX are using different approaches, so we need both
 ]
 include_from_dir(genxsubmod_path, ".jl", genx_to_exclude)
