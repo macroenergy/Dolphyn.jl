40ee041d1eed4d194612518d4a6b0a6c804b0ecc
dharik13 <dharik13@gmail.com>
2024-04-25 11:04:39 -0400

    Updated voll value for HSC
    Updated the code to read the correct value of lost load for HSC sector.  Previously, code was reading the value for the power sector.

--- src/HSC/load_inputs/load_h2_demand.jl
+++ src/HSC/load_inputs/load_h2_demand.jl
@@ -48,9 +48,10 @@ function load_h2_demand(setup::Dict, path::AbstractString, sep::AbstractString, 
     inputs_load["pMax_H2_D_Curtail"] = zeros(H2_SEG)
     for s in 1:H2_SEG
         # Cost of each segment reported as a fraction of value of non-served energy - scaled implicitly
-        inputs_load["pC_H2_D_Curtail"][s] = collect(skipmissing(H2_load_in[!,:Cost_of_Demand_Curtailment_per_Tonne]))[s]*inputs_load["Voll"][1]
+        inputs_load["pC_H2_D_Curtail"][s] = collect(skipmissing(H2_load_in[!,:Cost_of_Demand_Curtailment_per_Tonne]))[s]*inputs_load["H2_Voll"][1]
         # Maximum hourly demand curtailable as % of the max demand (for each segment)
         inputs_load["pMax_H2_D_Curtail"][s] = collect(skipmissing(H2_load_in[!,:Max_Demand_Curtailment]))[s]
+
     end
     
     print_and_log("HSC_load_data.csv Successfully Read!")
