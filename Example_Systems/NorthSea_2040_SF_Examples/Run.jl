3f4d2a99c761aab1e5d9effeb8ea30fa4d1618aa
Ruaridh Macdonald <rmacd@mit.edu>
2024-05-16 15:09:42 -0400

    Remove example results, license duplicates, and result comparisons

--- Example_Systems/NorthSea_2040_SF_Examples/Run.jl
+++ Example_Systems/NorthSea_2040_SF_Examples/Run.jl
@@ -1,19 +1,3 @@
-"""
-DOLPHYN: Decision Optimization for Low-carbon for Power and Hydrogen Networks
-Copyright (C) 2023,  Massachusetts Institute of Technology
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-A complete copy of the GNU General Public License v2 (GPLv2) is available
-in LICENSE.txt.  Users uncompressing this from an archive may not have
-received this license file.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 using Dolphyn
 
 # The directory containing your settings folder and files
@@ -90,7 +74,3 @@ end
 if mysetup["ModelLiquidFuels"] == 1
     write_liquid_fuels_outputs(EP, outpath_GenX, mysetup, myinputs)
 end
-
-compare_results(outpath_GenX, joinpath(inputs_path, "Results_Example"))
-
-
--- Example_Systems/SmallNewEngland/ThreeZones_Gurobi/Run.jl
+++ Example_Systems/SmallNewEngland/ThreeZones_Gurobi/Run.jl
@@ -1,19 +1,3 @@
-"""
-DOLPHYN: Decision Optimization for Low-carbon Power and Hydrogen Networks
-Copyright (C) 2022,  Massachusetts Institute of Technology
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-A complete copy of the GNU General Public License v2 (GPLv2) is available
-in LICENSE.txt.  Users uncompressing this from an archive may not have
-received this license file.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 using Dolphyn
 using Gurobi
 
@@ -72,5 +56,3 @@ outpath_GenX = write_outputs(EP, outpath, mysetup, myinputs)
 if mysetup["ModelH2"] == 1
     write_HSC_outputs(EP, outpath_GenX, mysetup, myinputs)
 end
-
-compare_results(outpath_GenX, joinpath(inputs_path, "Results_Example"))
