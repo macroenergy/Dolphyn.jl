a385502152e4ef78b5b0c9b7ffa497f8ff3f2918
Junwenlaw <97320660+Junwenlaw@users.noreply.github.com>
2024-05-24 22:23:31 -0400

    Updated run file

--- Example_Systems/Eastern_US_MACRO/ThreeZones/Run.jl
+++ Example_Systems/Eastern_US_MACRO/ThreeZones/Run.jl
@@ -45,28 +45,7 @@ OPTIMIZER = configure_solver(mysetup["Solver"], settings_path, Gurobi.Optimizer)
 # #### Running a case
 
 # ### Load inputs
-myinputs = Dict() # myinputs dictionary will store read-in data and computed parameters
-myinputs = load_inputs(mysetup, inputs_path)
-
-# ### Load H2 inputs if modeling the hydrogen supply chain
-if mysetup["ModelH2"] == 1
-    myinputs = load_h2_inputs(myinputs, mysetup, inputs_path)
-end
-
-# ### Load CO2 inputs if modeling the carbon supply chain
-if mysetup["ModelCSC"] == 1
-    myinputs = load_co2_inputs(myinputs, mysetup, inputs_path)
-end
-
-### Load LF inputs if modeling the synthetic fuels supply chain
-if mysetup["ModelLiquidFuels"] == 1
-    myinputs = load_liquid_fuels_inputs(myinputs, mysetup, inputs_path)
-end
-
-### Load BESC inputs if modeling the bioenergy fuels supply chain
-if mysetup["ModelBESC"] == 1
-    myinputs = load_bio_inputs(myinputs, mysetup, inputs_path)
-end
+myinputs = load_all_inputs(mysetup, inputs_path)
 
 # ### Generate model
 EP = generate_model(mysetup, myinputs, OPTIMIZER)
@@ -122,27 +101,4 @@ myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
 ### Write power system output
 
 print_and_log("Writing Output")
-outpath = joinpath(inputs_path,"Results")
-outpath_GenX=write_outputs(EP, outpath, mysetup, myinputs)
-
-# Write hydrogen supply chain outputs
-if mysetup["ModelH2"] == 1
-    write_HSC_outputs(EP, outpath_GenX, mysetup, myinputs)
-end
-
-# Write carbon supply chain outputs
-if mysetup["ModelCSC"] == 1
-    write_CSC_outputs(EP, outpath_GenX, mysetup, myinputs)
-end
-
-# Write liquid fuels supply chain outputs
-if mysetup["ModelLiquidFuels"] == 1
-    write_liquid_fuels_outputs(EP, outpath_GenX, mysetup, myinputs)
-end
-
-# Write bioenergy fuels supply chain outputs
-if mysetup["ModelBESC"] == 1
-    write_bio_outputs(EP, outpath_GenX, mysetup, myinputs)
-end
-
-compare_results(outpath_GenX, joinpath(inputs_path, "Results_Example"))
+write_all_outputs(EP,mysetup, myinputs, inputs_path)
