b39e39fcc536090907d1d27813f3b162222ba031
dharik13 <dharik13@gmail.com>
2024-10-08 20:06:59 -0400

    Writing TMR prices for annual with mulitple years
    Modified code to write TMR prices with annual matching requirement when modeling multiple years

--- src/HSC/write_outputs/write_h2_tmr_prices.jl
+++ src/HSC/write_outputs/write_h2_tmr_prices.jl
@@ -24,6 +24,8 @@ function write_h2_tmr_prices(path::AbstractString, sep::AbstractString, inputs::
 	dfGen = inputs["dfGen"] # Power sector inputs
 	dfH2Gen = inputs["dfH2Gen"]
 	T = inputs["T"]     # Number of time steps (hours)
+	Rep_Periods = inputs["REP_PERIOD"] # number of representative periods
+
 
 	# Identify number of time matching requirements
 	nH2_TMR = count(s -> startswith(String(s), "H2_TMR_"), names(dfGen))
@@ -35,14 +37,14 @@ function write_h2_tmr_prices(path::AbstractString, sep::AbstractString, inputs::
 	# Electricity price: Dual variable of hourly power balance constraint = hourly price
 		dfPrice = DataFrame(TMR = 1:nH2_TMR) # The unit is $/MWh
 
-	# Dividing dual variable for each hour with corresponding hourly weight to retrieve marginal cost of generation
+	# Dividing dual variable for each hour with corresponding hourly price
 		if setup["ParameterScale"] == 1
 			dfPrice = hcat(dfPrice, DataFrame(dual.(EP[:cH2TMR])./transpose(inputs["omega"]*ModelScalingFactor), :auto))
 		else
 			dfPrice = hcat(dfPrice, DataFrame(dual.(EP[:cH2TMR])./transpose(inputs["omega"]), :auto))
 		end
 
-		auxNew_Names=[Symbol("Zone");[Symbol("t$t") for t in 1:T]]
+		auxNew_Names=[Symbol("TMR_number");[Symbol("t$t") for t in 1:T]]
 		rename!(dfPrice,auxNew_Names)
 
 		## Linear configuration final output
@@ -50,16 +52,27 @@ function write_h2_tmr_prices(path::AbstractString, sep::AbstractString, inputs::
 	
 
 	else # annual time matching scenario (TimeMatchingRequirement = 3)
-	
-		dfPrice = DataFrame(TMR_Price = convert(Array{Union{Missing, Float64}}, dual.(EP[:cH2TMR_Annual])))
+		if setup["MultipleYears"]==0
+			dfPrice = DataFrame(TMR_Price = convert(Array{Union{Missing, Float64}}, dual.(EP[:cH2TMR_Annual])))
 
 
-		if setup["ParameterScale"] == 1
-			dfPrice[!,:TMR_Price] = dfPrice[!,:TMR_Price] * ModelScalingFactor # Converting MillionUS$/GWh to US$/MWh
+			if setup["ParameterScale"] == 1
+				dfPrice[!,:TMR_Price] = dfPrice[!,:TMR_Price] * ModelScalingFactor # Converting MillionUS$/GWh to US$/MWh
+
+			end
+
+		else # MultipleYears - multiple annual time matching requirements - one for each year
+			dfTMR = DataFrame(TMR_number = 1:nH2_TMR) 
+
+			# Dividing dual variable for each hour with corresponding hourly weight to retrieve marginal cost of generation
+			if setup["ParameterScale"] == 1
+				dfPrice = hcat(dfTMR, DataFrame(Dict(Symbol("p$t") => dual.(EP[:cH2TMR_Annual])[:,t]./ModelScalingFactor for t=1:Rep_Periods)))
+			else
+				dfPrice = hcat(dfTMR, DataFrame(Dict(Symbol("p$t") => dual.(EP[:cH2TMR_Annual])[:,t] for t=1:Rep_Periods)))
+			end
 		end
 	
-	
-	CSV.write(string(path,sep,"TMR_prices.csv"), dfPrice)
+		CSV.write(string(path,sep,"TMR_prices.csv"), dftranspose(dfPrice,false),writeheader=false)
 
 	end
 
